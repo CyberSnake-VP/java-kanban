ebe5c60a35e6854b0a68f25574bb56e8e3e161ad
Vadim Pimenov <zigmundfreid89@yandex.ru>
2025-02-20 12:09:57 +0300

    Переопределить методы для создания, удаления, изменения задачи в классе FileBackedTaskManager

--- src/tasktracker/manager/FileBackedTaskManager.java
+++ src/tasktracker/manager/FileBackedTaskManager.java
@@ -1,10 +1,14 @@
 package tasktracker.manager;
 
 import tasktracker.exceptions.ManagerSaveException;
-import tasktracker.status.Status;
 import tasktracker.tasks.*;
-
-import java.io.*;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.Writer;
+import java.io.FileWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
@@ -16,7 +20,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
     }
 
     public void save() {
-        String taskForWrite = getStringTasks();
+        String taskForWrite = getAllTasksToFile();
         try (Writer fw = new FileWriter(data)) {
             fw.write(taskForWrite);
         } catch (FileNotFoundException e) {
@@ -27,7 +31,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
 
     }
 
-    private String getStringTasks() {
+    private String getAllTasksToFile() {
         StringBuilder total = new StringBuilder();
         String title = "id,type,name,status,description,epic";
         total.append(title).append("\n");
@@ -47,9 +51,9 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         return total.toString();
     }
 
-    public String toString(Task task) {
-        String type = TypeTasks.TASK.name();
+    private String toString(Task task) {
         String id = Integer.toString(task.getId());
+        String type = TypeTasks.TASK.name();
         String name = task.getName();
         String description = task.getDescription();
         String status = task.getStatus().toString();
@@ -84,6 +88,76 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         super.createSubtask(subtask);
         save();
         return subtask;
+    }
+
+    @Override
+    public boolean updateTask(Task task) {
+        boolean result = super.updateTask(task);
+        save();
+        return result;
+    }
+
+    @Override
+    public boolean updateEpic(Epic epic) {
+        boolean result = super.updateEpic(epic);
+        save();
+        return result;
+    }
+
+    @Override
+    public boolean updateSubtask(Subtask subtask) {
+        boolean result = super.updateSubtask(subtask);
+        save();
+        return result;
+    }
+
+    @Override
+    public Task deleteTask(int id) {
+        Task deleted = super.deleteTask(id);
+        save();
+        return deleted;
+    }
+
+    @Override
+    public Epic deleteEpic(int id) {
+        Epic deleted = super.deleteEpic(id);
+        save();
+        return deleted;
+    }
+
+    @Override
+    public Subtask deleteSubtask(int id) {
+        Subtask deleted = super.deleteSubtask(id);
+        save();
+        return deleted;
+    }
+
+    @Override
+    public ArrayList<Task> deleteTaskList() {
+        ArrayList<Task> deletedList = super.deleteTaskList();
+        save();
+        return deletedList;
+    }
+
+    @Override
+    public ArrayList<Epic> deleteEpicList() {
+        ArrayList<Epic> deletedList = super.deleteEpicList();
+        save();
+        return deletedList;
+    }
+
+    @Override
+    public ArrayList<Subtask> deleteSubtaskList() {
+        ArrayList<Subtask> deletedList = super.deleteSubtaskList();
+        save();
+        return deletedList;
+    }
+
+    @Override
+    public ArrayList<Subtask> deleteSubtaskList(Epic epic) {
+        ArrayList<Subtask> deletedList = super.deleteSubtaskList(epic);
+        save();
+        return deletedList;
     }
 
 }
